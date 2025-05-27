661e641c56d882f81209048a3ec63288624ba8c1
juangiron2 <143913201+juangiron2@users.noreply.github.com>
2025-05-26 23:53:03 -0500

    Update crud_interface.py

--- interfaces/crud_interface.py
+++ interfaces/crud_interface.py
@@ -1,1 +1,14 @@
+from abc import ABC, abstractmethod
 
+class CRUD(ABC):
+    @abstractmethod
+    def crear(self, obj): pass
+
+    @abstractmethod
+    def obtener_por_id(self, id): pass
+
+    @abstractmethod
+    def actualizar(self, obj): pass
+
+    @abstractmethod
+    def eliminar(self, id): pass
