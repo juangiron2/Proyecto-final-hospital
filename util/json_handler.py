2d0c17bcb3f852e76577f45e26ae8e76736b831c
juangiron2 <143913201+juangiron2@users.noreply.github.com>
2025-05-26 23:56:07 -0500

    Update json_handler.py

--- util/json_handler.py
+++ util/json_handler.py
@@ -1,1 +1,12 @@
+import json
+import os
 
+def guardar_json(ruta, datos):
+    with open(ruta, 'w', encoding='utf-8') as archivo:
+        json.dump(datos, archivo, indent=4, ensure_ascii=False)
+
+def cargar_json(ruta):
+    if not os.path.exists(ruta):
+        return {}
+    with open(ruta, 'r', encoding='utf-8') as archivo:
+        return json.load(archivo)
