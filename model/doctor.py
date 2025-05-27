c5ed215cfe27117faea98a433eb0d9860608441d
juangiron2 <143913201+juangiron2@users.noreply.github.com>
2025-05-26 23:53:28 -0500

    Update doctor.py

--- model/doctor.py
+++ model/doctor.py
@@ -1,1 +1,8 @@
+class Doctor:
+    def __init__(self, id_doctor, nombre, especialidad):
+        self.id = id_doctor
+        self.nombre = nombre
+        self.especialidad = especialidad
 
+    def __str__(self):
+        return f"Doctor(ID: {self.id}, Nombre: {self.nombre}, Especialidad: {self.especialidad})"
